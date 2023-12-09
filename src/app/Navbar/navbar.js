aa2ecd0dfcbc8c0bd26712d66619ba8409669c4a
Samir Jadhav <samirzjadhav@gmail.com>
2023-12-09 19:59:09 +0530

    init

---
+++ src/app/Navbar/navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { HiChevronDown } from "react-icons/hi";
+
+const navbar = () => {
+  return (
+    <section className="navbar">
+      <div className="nav-container flex justify-between p-[10px]">
+        <img src="./logo.svg" alt="logo" width={200} height={50} />
+
+        <div className="nav-section flex items-center gap-[20px]">
+          <button className="nav-button flex justify-center items-center">
+            <span>Use Cases</span>
+            <HiChevronDown />
+          </button>
+          <button className="nav-button flex justify-center items-center">
+            <span>Product</span>
+            <HiChevronDown />
+          </button>
+          <span className="">
+            <a href="#">Customer</a>
+          </span>
+          <span className="">
+            <a href="#">Pricing</a>
+          </span>
+          <span className="flex gap-[20px]">
+            <a href="#">Sign in</a>
+            <a href="#">Get Supademo for Free</a>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default navbar;
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Navbar from "./Navbar/navbar.js";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
