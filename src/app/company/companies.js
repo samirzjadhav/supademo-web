9a38dbf79434827de22e739c13cb31eadc0a9d69
Samir Jadhav <samirzjadhav@gmail.com>
2023-12-15 22:50:58 +0530

    feat: added logos

---
+++ src/app/company/companies.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+function companies() {
+  return (
+    <section className="company  max-w-7xl mr-auto ml-auto text-center py-[64px]">
+      <div className="company-title mb-[40px]">
+        {" "}
+        <h1>Trusted by 7000+ users at fast-growing companies</h1>
+      </div>
+      <div className="company-logos flex gap-[20px]">
+        <a href="#">
+          <img
+            src="./company-logo-1.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-2.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-3.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-4.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-5.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-6.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-7.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="./company-logo-8.svg
+        "
+            className="w-[150px] h-[50px]"
+            alt="Company Logo"
+          />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default companies;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./Navbar/navbar.js";
 import Hero from "./Hero/hero.js";
+import Company from "./Company/companies.js";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Company />
     </div>
   );
 }
