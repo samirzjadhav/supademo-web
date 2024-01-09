151725873a14a61744dffafcba28ba6b451ac1f4
Samir Jadhav <samirzjadhav@gmail.com>
2024-01-09 22:15:51 +0530

    fix: companies.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
 import Image from "next/image";
 import Navbar from "./Navbar/navbar.js";
 import Hero from "./Hero/hero.js";
-import Company from "./Company/companies.js";
 import Product from "./Product/product.js";
 import Testimonial from "./Testimonial/testimonial.js";
-
+import Demos from "./Demos/demos.js";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Company />
       <Product />
       <Testimonial />
     </div>
