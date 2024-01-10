700a37b63253ebe81ff70324f38b51d9772f9781
Samir Jadhav <samirzjadhav@gmail.com>
2024-01-10 22:55:05 +0530

    feat: added demos header and dis

--- src/app/Demos/demos.js
+++ src/app/Demos/demos.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function demos() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="max-w-7xl mr-auto ml-auto pb-[100px]  pt-[100px] flex flex-col items-center justify-center">
+      <h3 className="demo-header text-[36px] font-semibold">
+        Scale how your team demonstrates products
+      </h3>
+      <p>
+        Supademo makes product demos and guides quick and intuitive for everyone
+        on the team.
+      </p>
+    </section>
+  );
 }
 
-export default demos
+export default demos;
--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
       <Company />
       <Product />
       <Testimonial />
+      <Demos />
     </div>
   );
 }
