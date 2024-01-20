d330a602d9937006f5b117de7d62848be136dcb7
Samir Jadhav <samirzjadhav@gmail.com>
2024-01-20 22:17:04 +0530

    feat: added review section

--- src/app/Review/review.js
+++ src/app/Review/review.js
@@ -3,7 +3,17 @@ import Image from "next/image";
 
 function review() {
   return (
-    <section className="review max-w-7xl mr-auto ml-auto pt-[100px] pb-[100px]"></section>
+    <section className="review max-w-7xl mr-auto ml-auto pt-[100px] pb-[100px]">
+      <div className="review-container">
+        <div className="review-top flex flex-col justify-center items-center">
+          <h1>
+            Hands down the <span> best demo product we have used</span> so far.
+            It is like Loom but 10x more intuitive, embeddable and slick
+          </h1>
+          
+        </div>
+      </div>
+    </section>
   );
 }
 
