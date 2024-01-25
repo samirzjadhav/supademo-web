e35141fcc176ab6e60984d45f735fc295e36a9d7
Samir Jadhav <samirzjadhav@gmail.com>
2024-01-25 21:04:47 +0530

    feat: added people review

--- src/app/Review/review.js
+++ src/app/Review/review.js
@@ -1,6 +1,95 @@
 import React from "react";
 import Image from "next/image";
 
+import { HiArrowSmRight } from "react-icons/hi";
+
+const peopleReviewsList = [
+  {
+    id: 1,
+    name: "Daniela De Almada",
+    role: "Head of Marketing",
+    profileImg: "/daniela-photo.png",
+    review:
+      "I absolutely love Supademo. We are using it for so much more than just showcasing use cases in our home page!",
+  },
+  {
+    id: 2,
+    name: "Casey O'Brien",
+    role: "Solutions Consulting Director",
+    profileImg: "/daniela-photo.png",
+    review:
+      "Supademo has completely transformed our early-stage demo motion. Gone are the days of custom demos for unqualified prospects.",
+  },
+  {
+    id: 3,
+    name: "Felix True",
+    role: "Head of Presales",
+    profileImg: "/daniela-photo.png",
+    review:
+      "Supademo has been a huge asset across multiple departments and workflows at easy software. We are thrilled to be working with them.",
+  },
+  {
+    id: 4,
+    name: "Eric Reynolds",
+    role: "Marketing Agency Owner",
+    profileImg: "/daniela-photo.png",
+    review:
+      "Must needed extension! Supademo is the perfect tool for educators, web design agencies, software providers, and helpful for pretty much any individual, company, or organization that needs to train or provide step-by-step tutorials.",
+  },
+  {
+    id: 1,
+    name: "Justin Berg",
+    role: "Co-founder",
+    profileImg: "/daniela-photo.png",
+    review:
+      "A game changer for us when it comes to making our product even more simple for our users to grasp, as well as helping them during the actual onboarding process.",
+  },
+  {
+    id: 1,
+    name: "Justin Berg",
+    role: "Co-founder",
+    profileImg: "/daniela-photo.png",
+    review:
+      "A game changer for us when it comes to making our product even more simple for our users to grasp, as well as helping them during the actual onboarding process.",
+  },
+  {
+    id: 1,
+    name: "Justin Berg",
+    role: "Co-founder",
+    profileImg: "/daniela-photo.png",
+    review:
+      "A game changer for us when it comes to making our product even more simple for our users to grasp, as well as helping them during the actual onboarding process.",
+  },
+];
+
+const peopleReviews = peopleReviewsList.map((peopleReview) => (
+  <div
+    className="reviewer p-[20px] flex flex-col gap-[16px] rounded-2xl  bg-[#F1F5F9]"
+    key={peopleReview.id}
+  >
+    <div className="flex gap-[10px]">
+      <Image
+        src={peopleReview.profileImg}
+        alt="Daniela De Almada"
+        width={50}
+        height={50}
+        className="rounded-full"
+      />
+      <div className="reviewer">
+        <h4 className="font-bold text-[18px]">{peopleReview.name}</h4>
+        <h5 className="font-medium ">{peopleReview.role}</h5>
+      </div>
+    </div>
+    <div className="review-message w-[300px]">{peopleReview.review}</div>
+    <div className="review-button flex items-center justify-end gap-[4px] text-blue-400">
+      <button className="font-bold ">Learn More</button>
+      <HiArrowSmRight />
+    </div>
+  </div>
+));
+
+console.log(peopleReviews);
+
 function review() {
   return (
     <section className="review max-w-7xl mr-auto ml-auto pt-[100px] pb-[100px]">
@@ -30,6 +119,12 @@ function review() {
             </div>
           </div>
           <Image src="/review-badge-min.png" width={420} height={100} alt="" />
+        </div>
+
+        <div className="review-bottom flex justify-center items-center mt-[100px]  ">
+          <div className="review-info grid grid-cols-3 gap-5">
+            {peopleReviews}
+          </div>
         </div>
       </div>
     </section>
